1559b4e7e853f3ecd6dbcb637c1a95361a9de781
Galactic-Cat <11347559+Galactic-Cat@users.noreply.github.com>
2022-11-22 14:29:33 +0100

    Removed some old experiments

--- experiments/pydual/identifier.py
+++
@@ -1,7 +0,0 @@
-idx = -1
-
-def get_idx() -> int:
-    global idx
-
-    idx += 1
-    return idx
--- experiments/pydual/test.py
+++
@@ -1,7 +0,0 @@
-from expression import Addition, Variable
-
-x = Variable()
-y = Variable()
-t = Addition(Addition(x, y), x)
-
-print(t.tree())
